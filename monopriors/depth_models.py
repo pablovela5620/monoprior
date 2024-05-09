607e32cd1c899d975dd23c41c9e59e85a411996b
pablovela5620 <pablovela5620@gmail.com>
2024-05-09 14:04:55 -0500

    refactor: make Unidepth work with V1 and V2

--- monopriors/depth_models.py
+++ monopriors/depth_models.py
@@ -2,18 +2,31 @@ from typing import Literal
 import torch
 import numpy as np
 from jaxtyping import Float, UInt8
+from timeit import default_timer as timer
 
 
 class UniDepthPredictor:
-    def __init__(self, device: Literal["cpu", "cuda"]):
+    def __init__(
+        self,
+        device: Literal["cpu", "cuda"],
+        version: Literal["v1", "v2"] = "v2",
+        backbone: Literal["cnvnxtl", "vitl14"] = "vitl14",
+    ):
+        self.k_output_key: str = "intrinsics" if version == "v1" else "K"
+        if version == "v2":
+            assert backbone == "vitl14", "Only vitl14 backbone is supported for v2"
+
+        print("Loading UniDepth model...")
+        start = timer()
         self.model = torch.hub.load(
             "lpiccinelli-eth/UniDepth",
             "UniDepth",
-            version="v1",
-            backbone="ConvNextL",
+            version=version,
+            backbone=backbone,
             pretrained=True,
             trust_repo=True,
         ).to(device)
+        print(f"UniDepth model loaded. Time: {timer() - start:.2f}s")
 
     def __call__(
         self, rgb: UInt8[np.ndarray, "h w 3"], K_33: Float[np.ndarray, "3 3"] | None
@@ -28,5 +41,5 @@ class UniDepthPredictor:
             predictions = self.model.infer(rgb, K_33)
 
         depth_b1hw: Float[torch.Tensor, "b 1 h w"] = predictions["depth"]
-        K_b33: Float[torch.Tensor, "b 3 3"] = predictions["intrinsics"]
+        K_b33: Float[torch.Tensor, "b 3 3"] = predictions[self.k_output_key]
         return depth_b1hw, K_b33
