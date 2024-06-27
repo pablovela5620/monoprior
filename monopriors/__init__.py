a3c37e09545293899e36e7761569a24791c44865
pablovela5620 <pablovela5620@gmail.com>
2024-06-26 20:40:35 -0500

    add beartype annotations

--- monopriors/__init__.py
+++ monopriors/__init__.py
@@ -1,16 +1,3 @@
-# from beartype.claw import beartype_this_package  # <-- boilerplate for victory
+from beartype.claw import beartype_this_package
 
-# beartype_this_package()
-from beartype.claw import beartype_package
-from beartype import BeartypeConf
-
-# Exclude the third_party directory by not applying beartype on it
-submodules_to_check: list[str] = [
-    "monopriors.relative_depth_models",
-    # add other submodules except the ones in third_party
-]
-
-conf = BeartypeConf()  # Customize the configuration if needed
-
-for submodule in submodules_to_check:
-    beartype_package(submodule, conf=conf)
+beartype_this_package()
