1c28fadac3b2fc7e9fb6a79c3ad750495c04b73f
pablovela5620 <pablovela5620@gmail.com>
2024-07-02 12:29:13 -0500

    move clip_dispairty funciton to utils, add edge mask function, clean up depth->points

--- monopriors/depth_utils.py
+++ monopriors/depth_utils.py
@@ -1,5 +1,6 @@
-from jaxtyping import Float32
+from jaxtyping import Float32, Int64, Bool
 import numpy as np
+from einops import rearrange
 
 
 def estimate_intrinsics(
@@ -40,38 +41,76 @@ def depth_to_disparity(
     return disparity
 
 
+def depth_edges_mask(
+    depth: Float32[np.ndarray, "h w"], threshold: float = 0.1
+) -> Bool[np.ndarray, "h w"]:
+    """Returns a mask of edges in the depth map.
+    Args:
+    depth: 2D numpy array of shape (H, W) with dtype float32.
+    Returns:
+    mask: 2D numpy array of shape (H, W) with dtype bool.
+    """
+    # Compute the x and y gradients of the depth map.
+    depth_dx, depth_dy = np.gradient(depth)
+    # Compute the gradient magnitude.
+    depth_grad = np.sqrt(depth_dx**2 + depth_dy**2)
+    # Compute the edge mask.
+    mask: Bool[np.ndarray, "h w"] = depth_grad > threshold
+    return mask
+
+
 def depth_to_points(
     depth_1hw: Float32[np.ndarray, "1 h w"],
     K_33: Float32[np.ndarray, "3 3"],
     R=None,
     t=None,
-):
+) -> Float32[np.ndarray, "h w 3"]:
     K_33_inv: Float32[np.ndarray, "3 3"] = np.linalg.inv(K_33)
     if R is None:
         R: Float32[np.ndarray, "3 3"] = np.eye(3, dtype=np.float32)
     if t is None:
-        t = np.zeros(3, dtype=np.float32)
-
-    M = np.eye(3, dtype=np.float32)
+        t: Float32[np.ndarray, "3"] = np.zeros(3, dtype=np.float32)
 
     _, height, width = depth_1hw.shape
 
-    x = np.arange(width)
-    y = np.arange(height)
-    coord = np.stack(np.meshgrid(x, y), -1)
-    coord = np.concatenate((coord, np.ones_like(coord)[:, :, [0]]), -1)  # z=1
-    coord = coord.astype(np.float32)
-    # coord = torch.as_tensor(coord, dtype=torch.float32, device=device)
-    coord = coord[None]  # bs, h, w, 3
+    # create 3d points grid
+    x: Int64[np.ndarray, " h"] = np.arange(width)
+    y: Int64[np.ndarray, " w"] = np.arange(height)
+    coord: Int64[np.ndarray, "h w 2"] = np.stack(np.meshgrid(x, y), -1)
+    z: Int64[np.ndarray, "h w 1"] = np.ones_like(coord)[:, :, [0]]
+    coord = np.concatenate((coord, z), -1).astype(np.float32)  # z=1
+    coord: Float32[np.ndarray, "1 h w 3"] = rearrange(coord, "h w c -> 1 h w c")
 
-    D = depth_1hw[:, :, :, None, None]
-    # print(D.shape, Kinv[None, None, None, ...].shape, coord[:, :, :, :, None].shape )
-    pts3D_1 = D * K_33_inv[None, None, None, ...] @ coord[:, :, :, :, None]
-    # pts3D_1 live in your coordinate system. Convert them to Py3D's
-    pts3D_1 = M[None, None, None, ...] @ pts3D_1
-    # from reference to targe tviewpoint
-    pts3D_2 = R[None, None, None, ...] @ pts3D_1 + t[None, None, None, :, None]
-    # pts3D_2 = pts3D_1
-    # depth_2 = pts3D_2[:, :, :, 2, :]  # b,1,h,w
-    print(pts3D_2.shape, pts3D_2.shape)
-    return pts3D_2[:, :, :, :3, 0][0]
+    # from depth to 3D points
+    depth_1hw11: Float32[np.ndarray, "1 h w 1 1"] = rearrange(
+        depth_1hw, "1 h w -> 1 h w 1 1"
+    )
+
+    # back project points from pixels to camera coordinate system
+    pts3D_1 = (
+        depth_1hw11
+        * rearrange(K_33_inv, "h w -> 1 1 1 h w")
+        @ rearrange(coord, "1 h w c -> 1 h w c 1")
+    )
+
+    # transform from camera to world coordinate system
+    pts3D_2: Float32[np.ndarray, "1 h w 3 1"] = rearrange(
+        R, "h w -> 1 1 1 h w"
+    ) @ pts3D_1 + rearrange(t, "s -> 1 1 1 s 1")
+
+    # rearrange to 3D points
+    pointcloud: Float32[np.ndarray, "h w 3"] = rearrange(pts3D_2, "1 h w c 1 -> h w c")
+    return pointcloud
+
+
+def clip_disparity(disparity):
+    # Get the 95th percentile of the disparity values
+    p95 = np.percentile(disparity, 99)
+    # Clip the disparity values at 105% of the 95th percentile
+    clipped_disparity = np.clip(disparity, a_min=None, a_max=1.01 * p95)
+    # normalize the disparity to 0-1
+    clipped_disparity = (clipped_disparity - clipped_disparity.min()) / (
+        clipped_disparity.max() - clipped_disparity.min()
+    )
+    clipped_disparity = (clipped_disparity * 255).astype(np.uint8)
+    return clipped_disparity
