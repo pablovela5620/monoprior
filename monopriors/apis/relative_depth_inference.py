fa04feda154cb64455e5b3bb54a0125876fefcb0
pablo vela <pablovela5620@gmail.com>
2025-05-12 15:26:23 -0500

    feat: add blueprint setup for relative depth visualization and update logging sequence

--- monopriors/apis/relative_depth_inference.py
+++ monopriors/apis/relative_depth_inference.py
@@ -5,6 +5,7 @@ import cv2
 import mmcv
 import numpy as np
 import rerun as rr
+import rerun.blueprint as rrb
 from simplecv.rerun_log_utils import RerunTyroConfig
 
 from monopriors.relative_depth_models import (
@@ -33,6 +34,19 @@ def resize_image(image: np.ndarray, max_dim: int = 1024) -> np.ndarray:
 
 def relative_depth_from_img(config: PredictorConfig) -> None:
     parent_log_path = Path("world")
+    blueprint = rrb.Blueprint(
+        rrb.Horizontal(
+            rrb.Spatial3DView(),
+            rrb.Vertical(
+                rrb.Spatial2DView(origin=f"{parent_log_path}/camera/pinhole/image"),
+                rrb.Spatial2DView(origin=f"{parent_log_path}/camera/pinhole/depth"),
+                rrb.Spatial2DView(origin=f"{parent_log_path}/camera/disparity"),
+            ),
+            column_shares=[3, 1],
+        ),
+        collapse_panels=True,
+    )
+    rr.send_blueprint(blueprint=blueprint)
     bgr_hw3 = cv2.imread(str(config.image_path))
     rgb_hw3 = cv2.cvtColor(bgr_hw3, cv2.COLOR_BGR2RGB)
 
@@ -41,5 +55,6 @@ def relative_depth_from_img(config: PredictorConfig) -> None:
 
     predictor: BaseRelativePredictor = get_relative_predictor(config.predictor_name)(device="cuda")
     relative_pred: RelativeDepthPrediction = predictor.__call__(rgb=rgb_hw3, K_33=None)
-    rr.log(f"{parent_log_path}", rr.ViewCoordinates.RDF, static=True)
+    rr.set_time("time", sequence=0)
+    rr.log("/", rr.ViewCoordinates.RDF, static=True)
     log_relative_pred(parent_log_path, relative_pred, rgb_hw3, depth_edge_threshold=0.1)
