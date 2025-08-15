908070e2879a3a7cdf4042351b4d1a557f4c853b
pablo vela <pablovela5620@gmail.com>
2025-08-14 19:50:37 -0500

    Add initial camera utilities and depth utilities
    - Implement camera transformation helper functions in camera_utils.py
    - Add functions for estimating intrinsics and converting disparity to depth in depth_utils.py
    - Update pixi.lock with new dependencies
    - Modify pyproject.toml to include new ignore rules for linters

---
+++ monopriors/camera_utils.py
@@ -0,0 +1,212 @@
+# Copyright 2022 the Regents of the University of California, Nerfstudio Team and contributors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Camera transformation helper code.
+"""
+
+import math
+from typing import Literal, NamedTuple
+
+import torch
+from jaxtyping import Float
+from torch import Tensor
+
+
+def rotation_matrix_between(a: Float[Tensor, "3"], b: Float[Tensor, "3"]) -> Float[Tensor, "3 3"]:
+    """Compute the rotation matrix that rotates vector a to vector b.
+
+    Args:
+        a: The vector to rotate.
+        b: The vector to rotate to.
+    Returns:
+        The rotation matrix.
+    """
+    a = a / torch.linalg.norm(a)
+    b = b / torch.linalg.norm(b)
+    v = torch.linalg.cross(a, b)  # Axis of rotation.
+
+    # Handle cases where `a` and `b` are parallel.
+    eps = 1e-6
+    if torch.sum(torch.abs(v)) < eps:
+        x = torch.tensor([1.0, 0, 0]) if abs(a[0]) < eps else torch.tensor([0, 1.0, 0])
+        v = torch.linalg.cross(a, x)
+
+    v = v / torch.linalg.norm(v)
+    skew_sym_mat = torch.Tensor(
+        [
+            [0, -v[2], v[1]],
+            [v[2], 0, -v[0]],
+            [-v[1], v[0], 0],
+        ]
+    )
+    theta = torch.acos(torch.clip(torch.dot(a, b), -1, 1))
+
+    # Rodrigues rotation formula. https://en.wikipedia.org/wiki/Rodrigues%27_rotation_formula
+    return torch.eye(3) + torch.sin(theta) * skew_sym_mat + (1 - torch.cos(theta)) * (skew_sym_mat @ skew_sym_mat)
+
+
+def focus_of_attention(poses: Float[Tensor, "*num_poses 4 4"], initial_focus: Float[Tensor, "3"]) -> Float[Tensor, "3"]:
+    """Compute the focus of attention of a set of cameras. Only cameras
+    that have the focus of attention in front of them are considered.
+
+     Args:
+        poses: The poses to orient.
+        initial_focus: The 3D point views to decide which cameras are initially activated.
+
+    Returns:
+        The 3D position of the focus of attention.
+    """
+    # References to the same method in third-party code:
+    # https://github.com/google-research/multinerf/blob/1c8b1c552133cdb2de1c1f3c871b2813f6662265/internal/camera_utils.py#L145
+    # https://github.com/bmild/nerf/blob/18b8aebda6700ed659cb27a0c348b737a5f6ab60/load_llff.py#L197
+    active_directions = -poses[:, :3, 2:3]
+    active_origins = poses[:, :3, 3:4]
+    # initial value for testing if the focus_pt is in front or behind
+    focus_pt = initial_focus
+    # Prune cameras which have the current have the focus_pt behind them.
+    active = torch.sum(active_directions.squeeze(-1) * (focus_pt - active_origins.squeeze(-1)), dim=-1) > 0
+    done = False
+    # We need at least two active cameras, else fallback on the previous solution.
+    # This may be the "poses" solution if no cameras are active on first iteration, e.g.
+    # they are in an outward-looking configuration.
+    while torch.sum(active.int()) > 1 and not done:
+        active_directions = active_directions[active]
+        active_origins = active_origins[active]
+        # https://en.wikipedia.org/wiki/Line–line_intersection#In_more_than_two_dimensions
+        m = torch.eye(3) - active_directions * torch.transpose(active_directions, -2, -1)
+        mt_m = torch.transpose(m, -2, -1) @ m
+        focus_pt = torch.linalg.inv(mt_m.mean(0)) @ (mt_m @ active_origins).mean(0)[:, 0]
+        active = torch.sum(active_directions.squeeze(-1) * (focus_pt - active_origins.squeeze(-1)), dim=-1) > 0
+        if active.all():
+            # the set of active cameras did not change, so we're done.
+            done = True
+    return focus_pt
+
+
+class OrientResults(NamedTuple):
+    oriented_poses: Float[Tensor, "*num_poses 3 4"]
+    transform: Float[Tensor, "3 4"]
+
+
+def auto_orient_and_center_poses(
+    world_T_cam_gl: Float[Tensor, "*num_poses 4 4"],
+    method: Literal["pca", "up", "vertical", "none"] = "up",
+    center_method: Literal["poses", "focus", "none"] = "poses",
+) -> OrientResults:
+    """Orients and centers the poses.
+
+    We provide three methods for orientation:
+
+    - pca: Orient the poses so that the principal directions of the camera centers are aligned
+        with the axes, Z corresponding to the smallest principal component.
+        This method works well when all of the cameras are in the same plane, for example when
+        images are taken using a mobile robot.
+    - up: Orient the poses so that the average up vector is aligned with the z axis.
+        This method works well when images are not at arbitrary angles.
+    - vertical: Orient the poses so that the Z 3D direction projects close to the
+        y axis in images. This method works better if cameras are not all
+        looking in the same 3D direction, which may happen in camera arrays or in LLFF.
+
+    There are two centering methods:
+
+    - poses: The poses are centered around the origin.
+    - focus: The origin is set to the focus of attention of all cameras (the
+        closest point to cameras optical axes). Recommended for inward-looking
+        camera configurations.
+
+    Args:
+        poses: The poses to orient.
+        method: The method to use for orientation.
+        center_method: The method to use to center the poses.
+
+    Returns:
+        Tuple of the oriented poses and the transform matrix.
+    """
+
+    origins = world_T_cam_gl[..., :3, 3]
+
+    mean_origin = torch.mean(origins, dim=0)
+    translation_diff = origins - mean_origin
+
+    if center_method == "poses":
+        translation = mean_origin
+    elif center_method == "focus":
+        translation = focus_of_attention(world_T_cam_gl, mean_origin)
+    elif center_method == "none":
+        translation = torch.zeros_like(mean_origin)
+    else:
+        raise ValueError(f"Unknown value for center_method: {center_method}")
+
+    if method == "pca":
+        _, eigvec = torch.linalg.eigh(translation_diff.T @ translation_diff)
+        eigvec = torch.flip(eigvec, dims=(-1,))
+
+        if torch.linalg.det(eigvec) < 0:
+            eigvec[:, 2] = -eigvec[:, 2]
+
+        transform = torch.cat([eigvec, eigvec @ -translation[..., None]], dim=-1)
+        oriented_poses = transform @ world_T_cam_gl
+
+        if oriented_poses.mean(dim=0)[2, 1] < 0:
+            oriented_poses[1:3, :] = -1 * oriented_poses[1:3, :]
+            transform[1:3, :] = -1 * transform[1:3, :]
+    elif method in ("up", "vertical"):
+        up = torch.mean(world_T_cam_gl[:, :3, 1], dim=0)
+        up = up / torch.linalg.norm(up)
+        if method == "vertical":
+            # If cameras are not all parallel (e.g. not in an LLFF configuration),
+            # we can find the 3D direction that most projects vertically in all
+            # cameras by minimizing ||Xu|| s.t. ||u||=1. This total least squares
+            # problem is solved by SVD.
+            x_axis_matrix = world_T_cam_gl[:, :3, 0]
+            _, S, Vh = torch.linalg.svd(x_axis_matrix, full_matrices=False)
+            # Singular values are S_i=||Xv_i|| for each right singular vector v_i.
+            # ||S|| = sqrt(n) because lines of X are all unit vectors and the v_i
+            # are an orthonormal basis.
+            # ||Xv_i|| = sqrt(sum(dot(x_axis_j,v_i)^2)), thus S_i/sqrt(n) is the
+            # RMS of cosines between x axes and v_i. If the second smallest singular
+            # value corresponds to an angle error less than 10° (cos(80°)=0.17),
+            # this is probably a degenerate camera configuration (typical values
+            # are around 5° average error for the true vertical). In this case,
+            # rather than taking the vector corresponding to the smallest singular
+            # value, we project the "up" vector on the plane spanned by the two
+            # best singular vectors. We could also just fallback to the "up"
+            # solution.
+            if S[1] > 0.17 * math.sqrt(world_T_cam_gl.shape[0]):
+                # regular non-degenerate configuration
+                up_vertical = Vh[2, :]
+                # It may be pointing up or down. Use "up" to disambiguate the sign.
+                up = up_vertical if torch.dot(up_vertical, up) > 0 else -up_vertical
+            else:
+                # Degenerate configuration: project "up" on the plane spanned by
+                # the last two right singular vectors (which are orthogonal to the
+                # first). v_0 is a unit vector, no need to divide by its norm when
+                # projecting.
+                up = up - Vh[0, :] * torch.dot(up, Vh[0, :])
+                # re-normalize
+                up = up / torch.linalg.norm(up)
+
+        rotation = rotation_matrix_between(up, torch.Tensor([0, 0, 1]))
+        transform = torch.cat([rotation, rotation @ -translation[..., None]], dim=-1)
+        oriented_poses = transform @ world_T_cam_gl
+    elif method == "none":
+        transform = torch.eye(4)
+        transform[:3, 3] = -translation
+        transform = transform[:3, :]
+        oriented_poses = transform @ world_T_cam_gl
+    else:
+        raise ValueError(f"Unknown value for method: {method}")
+
+    return OrientResults(oriented_poses=oriented_poses, transform=transform[:3, :])
--- monopriors/depth_utils.py
+++ monopriors/depth_utils.py
@@ -1,41 +1,45 @@
 import numpy as np
 from einops import rearrange
 from jaxtyping import Bool, Float32, Int64
+from numpy import ndarray
 
 
-def estimate_intrinsics(H: int, W: int, fov: float = 55.0) -> Float32[np.ndarray, "3 3"]:
+def estimate_intrinsics(H: int, W: int, fov: float = 55.0) -> Float32[ndarray, "3 3"]:
     """
     Intrinsics for a pinhole camera model from image dimensions.
     Assume fov of 55 degrees and central principal point.
     """
     f = 0.5 * W / np.tan(0.5 * fov * np.pi / 180.0)
-    cx = 0.5 * W
-    cy = 0.5 * H
-    K_33: Float32[np.ndarray, "3 3"] = np.array([[f, 0, cx], [0, f, cy], [0, 0, 1]], dtype=np.float32)
+    cx: float = 0.5 * W
+    cy: float = 0.5 * H
+    K_33: Float32[ndarray, "3 3"] = np.array([[f, 0, cx], [0, f, cy], [0, 0, 1]], dtype=np.float32)
     return K_33
 
 
 def disparity_to_depth(
-    disparity: Float32[np.ndarray, "h w"], focal_length: int, baseline: float = 1.0
-) -> Float32[np.ndarray, "h w"]:
+    disparity: Float32[ndarray, "h w"],
+    focal_length: int,
+    baseline: float = 1.0,
+    gamma: float = 2.2,
+) -> Float32[ndarray, "h w"]:
     range: float = float(np.minimum(disparity.max() / (disparity.min() + 1e-6), 100.0))
     disparity_max: float = float(disparity.max())
     min_disparity_range: float = disparity_max / range
 
-    depth: Float32[np.ndarray, "h w"] = (focal_length * baseline) / np.maximum(disparity, min_disparity_range)
+    depth: Float32[ndarray, "h w"] = (focal_length * baseline) / np.maximum(disparity, min_disparity_range)
     # gamma correction for better visualizationg
-    depth: Float32[np.ndarray, "h w"] = np.power(depth, 1.0 / 2.2)
+    depth: Float32[ndarray, "h w"] = np.power(depth, 1.0 / gamma)
     return depth
 
 
 def depth_to_disparity(
-    depth: Float32[np.ndarray, "h w"], focal_length: int, baseline: float = 1.0
-) -> Float32[np.ndarray, "h w"]:
+    depth: Float32[ndarray, "h w"], focal_length: int, baseline: float = 1.0
+) -> Float32[ndarray, "h w"]:
     disparity = (focal_length * baseline) / (depth + 0.01)
     return disparity
 
 
-def depth_edges_mask(depth: Float32[np.ndarray, "h w"], threshold: float = 0.1) -> Bool[np.ndarray, "h w"]:
+def depth_edges_mask(depth: Float32[ndarray, "h w"], threshold: float = 0.1) -> Bool[ndarray, "h w"]:
     """Returns a mask of edges in the depth map.
     Args:
     depth: 2D numpy array of shape (H, W) with dtype float32.
@@ -47,46 +51,67 @@ def depth_edges_mask(depth: Float32[np.ndarray, "h w"], threshold: float = 0.1) 
     # Compute the gradient magnitude.
     depth_grad = np.sqrt(depth_dx**2 + depth_dy**2)
     # Compute the edge mask.
-    mask: Bool[np.ndarray, "h w"] = depth_grad > threshold
+    mask: Bool[ndarray, "h w"] = depth_grad > threshold
     return mask
 
 
 def depth_to_points(
-    depth_1hw: Float32[np.ndarray, "1 h w"],
-    K_33: Float32[np.ndarray, "3 3"],
-    R=None,
-    t=None,
-) -> Float32[np.ndarray, "h w 3"]:
-    K_33_inv: Float32[np.ndarray, "3 3"] = np.linalg.inv(K_33)
-    if R is None:
-        R: Float32[np.ndarray, "3 3"] = np.eye(3, dtype=np.float32)
-    if t is None:
-        t: Float32[np.ndarray, "3"] = np.zeros(3, dtype=np.float32)
+    depth_1hw: Float32[ndarray, "1 h w"],
+    K_33: Float32[ndarray, "3 3"],
+    world_R_cam: Float32[ndarray, "3 3"] | None = None,
+    world_t_cam: Float32[ndarray, "3"] | None = None,
+) -> Float32[ndarray, "h w 3"]:
+    K_33_inv: Float32[ndarray, "3 3"] = np.linalg.inv(K_33)
+    if world_R_cam is None:
+        world_R_cam: Float32[ndarray, "3 3"] = np.eye(3, dtype=np.float32)
+    if world_t_cam is None:
+        world_t_cam: Float32[ndarray, "3"] = np.zeros(3, dtype=np.float32)
 
     _, height, width = depth_1hw.shape
 
     # create 3d points grid
-    x: Int64[np.ndarray, " h"] = np.arange(width)
-    y: Int64[np.ndarray, " w"] = np.arange(height)
-    coord: Int64[np.ndarray, "h w 2"] = np.stack(np.meshgrid(x, y), -1)
-    z: Int64[np.ndarray, "h w 1"] = np.ones_like(coord)[:, :, [0]]
+    x: Int64[ndarray, "w"] = np.arange(width)
+    y: Int64[ndarray, "h"] = np.arange(height)
+    coord: Int64[ndarray, "h w 2"] = np.stack(np.meshgrid(x, y), -1)
+    z: Int64[ndarray, "h w 1"] = np.ones_like(coord)[:, :, [0]]
     coord = np.concatenate((coord, z), -1).astype(np.float32)  # z=1
-    coord: Float32[np.ndarray, "1 h w 3"] = rearrange(coord, "h w c -> 1 h w c")
+    coord: Float32[ndarray, "1 h w 3"] = rearrange(coord, "h w c -> 1 h w c")
 
     # from depth to 3D points
-    depth_1hw11: Float32[np.ndarray, "1 h w 1 1"] = rearrange(depth_1hw, "1 h w -> 1 h w 1 1")
+    depth_1hw11: Float32[ndarray, "1 h w 1 1"] = rearrange(depth_1hw, "1 h w -> 1 h w 1 1")
 
     # back project points from pixels to camera coordinate system
     pts3D_1 = depth_1hw11 * rearrange(K_33_inv, "h w -> 1 1 1 h w") @ rearrange(coord, "1 h w c -> 1 h w c 1")
 
     # transform from camera to world coordinate system
-    pts3D_2: Float32[np.ndarray, "1 h w 3 1"] = rearrange(R, "h w -> 1 1 1 h w") @ pts3D_1 + rearrange(
-        t, "s -> 1 1 1 s 1"
+    pts3D_2: Float32[ndarray, "1 h w 3 1"] = rearrange(world_R_cam, "h w -> 1 1 1 h w") @ pts3D_1 + rearrange(
+        world_t_cam, "s -> 1 1 1 s 1"
     )
 
     # rearrange to 3D points
-    pointcloud: Float32[np.ndarray, "h w 3"] = rearrange(pts3D_2, "1 h w c 1 -> h w c")
+    pointcloud: Float32[ndarray, "h w 3"] = rearrange(pts3D_2, "1 h w c 1 -> h w c")
     return pointcloud
+
+
+def multidepth_to_points(
+    depth_maps: Float32[ndarray, "b h w 1"],
+    world_T_cam_batch: Float32[ndarray, "b 4 4"],
+    K_b33: Float32[ndarray, "b 3 3"],
+) -> Float32[ndarray, "b h w 3"]:
+    world_points_list: list[ndarray] = []
+    depth_map: Float32[ndarray, "h w 1"]
+    for idx, depth_map in enumerate(depth_maps):
+        world_T_cam: Float32[ndarray, "4 4"] = world_T_cam_batch[idx]
+        partial_points: Float32[ndarray, "h w 3"] = depth_to_points(
+            depth_1hw=rearrange(depth_map, "h w 1 -> 1 h w"),
+            K_33=K_b33[idx],
+            world_R_cam=world_T_cam[:3, :3],
+            world_t_cam=world_T_cam[:3, 3],
+        )
+        world_points_list.append(partial_points)
+
+    world_points: Float32[ndarray, "b h w 3"] = np.stack(world_points_list, axis=0)
+    return world_points
 
 
 def clip_disparity(disparity):
